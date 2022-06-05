1d09900e0e0d4e8e8fb03d5089b280b1a61314af
hyun0404woo <hyun0404woo@gmail.com>
2022-06-05 19:03:38 +0900

    Feat: Datadase Schema
    - Create Table
    - Insert Dummy data

---
+++ reactive-servlet/reactive/src/main/resources/data.sql
@@ -0,0 +1,27 @@
+
+INSERT INTO user (name, property) VALUES ('user1', 0.0);
+INSERT INTO user (name, property) VALUES ('user2', 0.0);
+INSERT INTO user (name, property) VALUES ('user3', 0.0);
+INSERT INTO user (name, property) VALUES ('user4', 0.0);
+INSERT INTO user (name, property) VALUES ('user5', 0.0);
+
+
+INSERT INTO stocktrade (
+    timestamp, company_name, price, trade_quantity, trade_action, user_id)
+    VALUES ('2022-06-05 18:00:00', '삼성', '65765.55', 1, 'BUY', 1);
+
+INSERT INTO stocktrade (
+    timestamp, company_name, price, trade_quantity, trade_action, user_id)
+VALUES ('2022-06-06 18:00:00', 'Kakao', '212121.22', 2, 'BUY', 2);
+
+INSERT INTO stocktrade (
+    timestamp, company_name, price, trade_quantity, trade_action, user_id)
+VALUES ('2022-06-07 18:00:00', 'Naver', '414141.41', 3, 'BUY', 3);
+
+INSERT INTO stocktrade (
+    timestamp, company_name, price, trade_quantity, trade_action, user_id)
+VALUES ('2022-06-08 18:00:00', 'SKT', '151515.15', 4, 'BUY', 4);
+
+INSERT INTO stocktrade (
+    timestamp, company_name, price, trade_quantity, trade_action, user_id)
+VALUES ('2022-06-09 18:00:00', 'Line', '71717.71', 5, 'BUY', 5);
